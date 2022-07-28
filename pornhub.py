b808d4c2e08d45d03b43ccfde112b73c02f0ad25
laoweilove <54768931+laoweilove@users.noreply.github.com>
2022-07-28 21:14:25 +0800

    update
    优化代码和变量名称

--- pornhub.py
+++ pornhub.py
@@ -1,5 +1,6 @@
 import requests
-import re,time
+import re
+import time
 from lxml import etree
 import execjs
 import pyaria2
@@ -7,114 +8,126 @@ from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util import Retry
 
 res = requests.Session()
-res.mount('https://', HTTPAdapter(max_retries=Retry(total=5, method_whitelist=frozenset(['GET', 'POST']))))
-rpc=pyaria2.Aria2RPC()#aria2rpc设置，默认6800端口，没密钥
+res.mount('https://', HTTPAdapter(max_retries=Retry(total=5, allowed_methods=frozenset(['GET', 'POST']))))
+rpc = pyaria2.Aria2RPC()  # aria2rpc设置，默认6800端口，没密钥
+lag = 'www'
+dic = {}
+proxy = {
+    'http':'http://127.0.0.1:7890',
+    'https': 'http://127.0.0.1:7890'
+}  # 我这里clash端口7890，v2ray 端口8001
 
-dic={}
-proxy={
-    #'http':'http://127.0.0.1:7890',
-    'https':'https://127.0.0.1:7890'
-} #我这里clashx端口7890，v2ray 端口8001
+cookie = open('cookie.txt', 'r').read()  # 自行粘贴cookie到同目录下cookie.txt
+user_agent = open('uag.txt', 'r').read()
+h = {
 
-cookie=open('cookie.txt','r').read()#自行粘贴cookie到同目录下cookie.txt
-h={
-
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.67',
+    'user-agent': user_agent,
     'cookie': cookie,
 }
 
-def download(chanels,s,name):
-    opin={'http-proxy':'http://127.0.0.1:7890','https-proxy':'https://127.0.0.1:7890','out':chanels+'/'+name}#aria2 设置梯子
-    rpc.addUri([s], opin)
 
-def getchannelslist(chanels,page):
-    url='https://cn.pornhub.com/channels/'+chanels+'/videos?o=ra&page='+str(page)
-    s=res.get(url,headers=h,proxies=proxy).text
-    html = etree.HTML(s)
-    vkeys = html.xpath('//ul[@class="videos row-5-thumbs videosGridWrapper"]/li/@data-video-vkey')
-    names=html.xpath('//ul[@class="videos row-5-thumbs videosGridWrapper"]/li/div/div[3]/span/a/@title')
-    for i,j in enumerate(vkeys):
-        getvideo(chanels,j,names[i])
-        print(j,'ok')
-    return len(vkeys)
+def download(channels, video_url, name):
+    options = {
+        'http-proxy': 'http://127.0.0.1:7890',
+        'https-proxy': 'http://127.0.0.1:7890',
+        'out': channels + '/' + name
+    }  # aria2 设置梯子
+    rpc.addUri([video_url], options)
 
-def starlist(star,page):
-    url='https://cn.pornhub.com/pornstar/'+star+'/videos/upload?page='+str(page)
+
+def channels_list(channels, page):
+    url = f'https://{lag}.pornhub.com/channels/{channels}/videos?o=ra&page={page}'
     s = res.get(url, headers=h, proxies=proxy).text
     html = etree.HTML(s)
-    vkeys = html.xpath('//ul[@class="videos row-5-thumbs"]/li/@data-video-vkey')
+    view_keys = html.xpath('//ul[@class="videos row-5-thumbs videosGridWrapper"]/li/@data-video-vkey')
+    names = html.xpath('//ul[@class="videos row-5-thumbs videosGridWrapper"]/li/div/div[3]/span/a/@title')
+    for i, j in enumerate(view_keys):
+        get_video(channels, j, names[i])
+        print(j, 'ok')
+    return len(view_keys)
+
+
+def star_list(pornstar, page):
+    url = f'https://{lag}.pornhub.com/pornstar/{pornstar}/videos/upload?page={page}'
+    s = res.get(url, headers=h, proxies=proxy).text
+    html = etree.HTML(s)
+    view_keys = html.xpath('//ul[@class="videos row-5-thumbs"]/li/@data-video-vkey')
     names = html.xpath('//ul[@class="videos row-5-thumbs"]/li/div/div[3]/span/a/@title')
-    for i, j in enumerate(vkeys):
-        getvideo(star, j, names[i])
+    for i, j in enumerate(view_keys):
+        get_video(pornstar, j, names[i])
         print(j, 'ok')
-    return len(vkeys)
+    return len(view_keys)
 
-def modellist(model,page):
-    url='https://cn.pornhub.com/model/'+model+'/videos?page='+str(page)
+
+def model_list(models, page):
+    url = f'https://{lag}.pornhub.com/model/{models}/videos?page={page}'
     s = res.get(url, headers=h, proxies=proxy).text
     html = etree.HTML(s)
-    vkeys = html.xpath('//ul[@id="mostRecentVideosSection"]/li/@data-video-vkey')
+    view_keys = html.xpath('//ul[@id="mostRecentVideosSection"]/li/@data-video-vkey')
     names = html.xpath('//ul[@id="mostRecentVideosSection"]/li/div/div[3]/span/a/@title')
-    for i, j in enumerate(vkeys):
-        getvideo(model, j, names[i])
+    for i, j in enumerate(view_keys):
+        get_video(models, j, names[i])
         print(j, 'ok')
-    return len(vkeys)
+    return len(view_keys)
 
-def getvideo(chanels,viewkey,name):
-    url='https://'+lag+'.pornhub.com/view_video.php?viewkey='+viewkey
+
+def get_video(channels, view_key, name):
+    url = f'https://{lag}.pornhub.com/view_video.php?viewkey={view_key}'
     try:
 
+        s = res.get(url, headers=h, timeout=5, proxies=proxy, ).text
+        js_data = re.findall('= media_\d;(var .*?media_\d.*?;)', s)
+        urls = []
+        for i, j in enumerate(js_data):
+            js = 'function test(a){ ' + j + 'return media_' + str(i + 1) + ';}'
+            ss = execjs.compile(js)
 
-        s=res.get(url,headers=h,timeout=5,proxies=proxy,).text
-        x=re.findall('= media_\d;(var .*?media_\d.*?;)',s)
-        urls=[]
-        for i,j in enumerate(x):
-            js='function test(a){ '+j+'return media_'+str(i+1)+';}'
-            ss=execjs.compile(js)
-
-            xnul=ss.call('test','1')
-            urls.append(xnul)
-        nul=urls[-1]
-        xx=''
-        lex=0
-        while xx=='':
-            xx=res.get(nul,headers=h,proxies=proxy).json()[lex-1]['videoUrl']
-            lex=lex-1
-            if lex <-3:
+            x_nul = ss.call('test', '1')
+            urls.append(x_nul)
+        nul = urls[-1]
+        final_url = ''
+        count = 0
+        while final_url == '':
+            final_url = res.get(nul, headers=h, proxies=proxy).json()[count - 1]['videoUrl']
+            count = count - 1
+            if count < -3:
                 break
 
-
-        download(chanels,xx,name+'.mp4')
+        download(channels, xx, name + '.mp4')
         time.sleep(2)
     except:
         pass
 
-def chanel(chanes):
-    lex=36
-    page=1
-    while lex==36:
-        lex=getchannelslist(chanes,page)
-        page+=1
 
-def star(chanes):
-    lex = 40
+def chanel(channels):
+    count = 36
     page = 1
-    while lex == 40:
-        lex = starlist(chanes, page)
+    while count == 36:
+        count = channels_list(channels, page)
         page += 1
 
-def model(chanes):
-    lex = 40
+
+def star(pornstar):
+    count = 40
     page = 1
-    while lex == 40:
-        lex = modellist(chanes, page)
+    while count == 40:
+        count = star_list(pornstar, page)
         page += 1
+
+
+def model(models):
+    count = 40
+    page = 1
+    while count == 40:
+        count = model_list(models, page)
+        page += 1
+
 
 if __name__ == '__main__':
-    x=input('地址').split('/')
-    if x[3]=='model':
-        model(x[4])
-    elif x[3]=='pornstar':
-        star(x[4])
-    elif x[3]=='channels':
-        chanel(x[4])
+    input_address = input('地址').split('/')
+    if input_address[3] == 'model':
+        model(input_address[4])
+    elif input_address[3] == 'pornstar':
+        star(input_address[4])
+    elif input_address[3] == 'channels':
+        chanel(input_address[4])
